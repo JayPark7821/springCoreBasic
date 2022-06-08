0ad3e58497cd1170565b7ac9f2d4de20ad1c3947
JayPark7821 <pjhyun1990@gmail.com>
2022-06-08 23:50:36 +0900

    새로운 할인정책 개발 & 테스트 ( 비율 )

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,20 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscouhntPolicy{
+
+    private int discountPercent = 10;
+
+    // ctrl + shift + t test생성
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP){
+            return price * discountPercent/100;
+        }else{
+            return 0;
+        }
+
+    }
+}
